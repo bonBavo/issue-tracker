915d7735c5aa13f07b6ac7d839b887138954a606
amulibraven <amulibraven254@gmail.com>
2025-03-18 20:53:46 +0300

    Api for creating an issue has been created

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { prisma } from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const issueBody = await request.json();
+  const validatedIssue = createIssueSchema.safeParse(issueBody);
+  if (!validatedIssue.success)
+    return NextResponse.json(validatedIssue.error.errors, { status: 404 });
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: issueBody.title,
+      description: issueBody.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
