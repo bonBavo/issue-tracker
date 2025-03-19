f096cc0eaf89960236b7084663cacd4c398c92ea
amulibraven <amulibraven254@gmail.com>
2025-03-19 15:07:10 +0300

    allows user to create an issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,15 +4,40 @@ import React from "react";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title">
-        <TextField.Slot></TextField.Slot>
-      </TextField.Root>
-      <SimpleMDE placeholder="type your issue in here" />
+    <form
+      className="max-w-xl space-y-3 "
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        render={({ field }) => (
+          <SimpleMDE placeholder="type your issue in here" {...field} />
+        )}
+        name="description"
+        control={control}
+      />
+
       <Button>submit new issue</Button>
-    </div>
+    </form>
   );
 };
 
